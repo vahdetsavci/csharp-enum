62e7fde0a97f62e6a7a1fc15b1d60f1461299224
Vahdet Savci <savcivahdet@gmail.com>
2023-11-13 16:32:01 +0300

    app is done

--- Program.cs
+++ Program.cs
@@ -1,8 +1,40 @@
-﻿namespace csharp_enum;
+﻿using System;
+
+namespace csharp_enum;
 
 class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine(Gunler.Pazar);
+        Console.WriteLine((int)Gunler.Cumartesi);
+
+        int sicaklik = 32;
+
+        if (sicaklik < (int)HavaDurumu.Normal)
+            Console.WriteLine("Dışarıya çıkmak için havanın biraz daha ısınmasını bekleyelim.");
+        else if (sicaklik >= (int)HavaDurumu.Sicak)
+            Console.WriteLine("Dışarıya çıkmak için sıcak bir gün");
+        else if (sicaklik >= (int)HavaDurumu.Normal && sicaklik < (int)HavaDurumu.Sicak)
+            Console.WriteLine("Hadi dışarıya çıkalım!");
     }
+}
+
+enum Gunler
+{
+    Pazartesi = 1,
+    Salı,
+    Çarşamba,
+    Perşembe,
+    Cuma = 23,
+    Cumartesi,
+    Pazar
+}
+
+enum HavaDurumu
+{
+    Soguk = 5,
+    Normal = 20,
+    Sicak = 25,
+    CokSicak = 30
 }
